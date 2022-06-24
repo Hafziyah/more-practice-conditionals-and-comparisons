ce294a061bf9de9b080cd0f1b3e1281ee3102b77
Hafsat_A <68890886+Hafziyah@users.noreply.github.com>
2022-06-24 05:16:43 +0100

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let stringNumber = "12";
+let numericalNumber = 12;
+
+//console.log(numericalNumber + 2);
+
+if (stringNumber === numericalNumber) {
+  console.log("True");
+} else if (stringNumber == numericalNumber) {
+  console.log("The values are equal");
+} else {
+  console.log("Neither condition is true");
+}
